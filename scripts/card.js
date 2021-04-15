797de9b717829c017fa3b4a94bc7dd722c19d31b
Jay Ozello <happyquackcrew@gmail.com>
2021-04-14 20:08:55 -0400

    Update card.js

--- scripts/card.js
+++ scripts/card.js
@@ -1,1 +1,27 @@
 
+class Card {
+  constructor(value, effect, rolling, imgPath) {
+    this.value = value;
+    this.effect = effect;
+    this.rolling = rolling;
+    this.imgPath = imgPath;
+  }
+  
+  getValue() {
+    return this.value;
+  }
+  
+  getEffect() {
+    return this.effect;
+  }
+  
+  isRolling() {
+    return this.rolling;
+  }
+  
+  getImg() {
+    img = document.createElement('img');
+    img.src = this.imgPath;
+    return img;
+  }
+}
