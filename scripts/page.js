63dd9e85d413fcb5b3931bb5837426999b2c442b
Jay Ozello <happyquackcrew@gmail.com>
2021-04-15 19:36:20 -0400

    Update page.js

--- scripts/page.js
+++ scripts/page.js
@@ -49,7 +49,7 @@ classIconImgs = [classSelection01, classSelection02, classSelection03,
                  classSelection13, classSelection14, classSelection15,
                  classSelection16, classSelection17, classSelection18];
 imageSideLength = PAGEWIDTH/8;
-Console.log(imageSideLength);
+Console.log(imageSideLength + "     SGUYUDGUYGHKJSGHJK");
 classSelectionBox.style.paddingLeft = imageSideLength;
 classSelectionBox.style.paddingRight = imageSideLength;
 for (let [index, image] of classIconImgs.entries()) {
