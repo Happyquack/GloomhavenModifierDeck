19f61056a49f1b7f2368c50f25ffc70b3c94589c
Jay Ozello <happyquackcrew@gmail.com>
2021-04-14 20:18:18 -0400

    Update deck.js

--- scripts/deck.js
+++ scripts/deck.js
@@ -1,3 +1,11 @@
-class Deck() {
+class Deck {
+  constructor(character) {
+    this.character = character
+    this.createBaseDeck()
+  }
   
+  createBaseDeck() {
+    this.deckArray = []
+    this.deckArray.push(new Card("null", 0, false, "../images/modifierDecks/baseCard01.png")
+  }
 }
