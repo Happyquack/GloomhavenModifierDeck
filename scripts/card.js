0db222f5e87c0410dd68df985756c53ca57bf8aa
Jay Ozello <happyquackcrew@gmail.com>
2021-04-16 22:49:29 -0400

    Update card.js

--- scripts/card.js
+++ scripts/card.js
@@ -36,7 +36,7 @@ class Card {
     this.imgCard.appendChild(back);
     this.imgCard.style.transition = "transform 0.6s";
     this.imgCard.style.transformStyle = "preserve-3d";
-    this.addEventListener("click", this.flip());
+    addEventListener("click", this.flip());
     return this.imgCard;
   }
   
