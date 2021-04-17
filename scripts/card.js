da980df9c0e6bcfc2ba055e62215b5ab0c12f57d
Jay Ozello <happyquackcrew@gmail.com>
2021-04-17 16:02:52 -0400

    Update card.js

--- scripts/card.js
+++ scripts/card.js
@@ -9,16 +9,14 @@ class Card {
     this.flipped = false;
     var img = document.createElement('img');
     img.src = this.imgPath;
-    img.style.transition = "transform 0.6s";
-    img.style.transformStyle = "preserve-3d";
     img.style.pointerEvents = "none";
     var back = document.createElement('img');
     back.src = this.backPath;
     back.style.transform = "rotateY(180deg)";
-    back.style.transition = "transform 0.6s";
-    back.style.transformStyle = "preserve-3d";
     back.style.pointerEvents = "none";
     this.imgCard = document.createElement('div');
+    this.imgCard.style.transition = "transform 0.6s";
+    this.imgCard.transformStyle = "preserve-3d";
     this.imgCard.appendChild(img);
     this.imgCard.appendChild(back);
     this.imgCard.addEventListener("click", this.flip);
