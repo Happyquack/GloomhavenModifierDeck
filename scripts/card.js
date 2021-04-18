2787ea9ddee125961e9e93e596dc9ff4afe1451e
Jay Ozello <happyquackcrew@gmail.com>
2021-04-17 23:00:28 -0400

    Update card.js

--- scripts/card.js
+++ scripts/card.js
@@ -50,10 +50,10 @@ class Card {
     console.log("FLIPPED");
     this.flipped = !this.flipped;
     if (this.flipped) {
-      this.imgCard.style.transform = "rotateY(180deg)";
+      //this.imgCard.style.transform = "rotateY(180deg)";
       this.outerCard.style.transform = "rotateY(180deg)";
     } else {
-      this.imgCard.style.transform = "rotateY(0deg)";
+      //this.imgCard.style.transform = "rotateY(0deg)";
       this.outerCard.style.transform = "rotateY(0deg)";
     }
     
