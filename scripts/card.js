f41c8884852a7bb210070e0ea9732e83b9ff017c
Jay Ozello <happyquackcrew@gmail.com>
2021-04-17 23:46:20 -0400

    Update card.js

--- scripts/card.js
+++ scripts/card.js
@@ -14,6 +14,7 @@ class Card {
     this.img.style.WebkitBackfaceVisibility = "hidden";
     this.img.style.transformStyle = "preserve-3d";
     this.img.style.opacity = "0.99";
+    this.img.style.transition = "transform 0.6s";
     this.back = document.createElement('img');
     this.back.src = this.backPath;
     this.back.style.transform = "rotateY(180deg)";
@@ -21,10 +22,11 @@ class Card {
     this.back.style.backfaceVisibility = "hidden";
     this.back.style.WebkitBackfaceVisibility = "hidden";
     this.back.style.transformStyle = "preserve-3d";
+    this.imgCard.style.transition = "transform 0.6s";
     this.img.style.opacity = "0.99";
     this.imgCard = document.createElement('div');
-    this.imgCard.style.transition = "transform 0.6s";
-    this.imgCard.transformStyle = "preserve-3d";
+    //this.imgCard.style.transition = "transform 0.6s";
+    //this.imgCard.transformStyle = "preserve-3d";
     this.imgCard.appendChild(this.img);
     this.imgCard.appendChild(this.back);
     this.outerCard = document.createElement('div')
