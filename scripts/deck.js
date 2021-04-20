a338a0fde5e946c1a8b1d40e20a86f04ae98947b
Jay Ozello <happyquackcrew@gmail.com>
2021-04-20 02:32:57 -0400

    Update deck.js

--- scripts/deck.js
+++ scripts/deck.js
@@ -73,7 +73,7 @@ class Deck {
     var rollingList = [0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0];
     this.loadDeck(this.characterDeck, cardDir, backDir, valueList, effectList, rollingList);
     this.perkList = ["x11-x12","x13-+0","+1-+2","+3-+4","+6","+7-+8-+9","+10-+11-+12","+13-+14","+15","+16","+17-+18","+19","+20","+21","+5"];
-    this.perkInstructions = ["1Remove two =-1= cards","1Replace one =-1= card with one =+1= card","1Add two =+1= cards","1Add one =+3= card","1Add two =rolling=pierce= 3 cards","2Add one =rolling=stun= card","1add one =rolling=disarm= and one =rolling=muddle= card","2Add one =+1=shield=1, Self card","Ignore negative item effects and add one =+1= card"];
+    this.perkInstructions = ["1Remove two =-1= cards","1Replace one =-1= card with one =+1= card","1Add two =+1= cards","1Add one =+3= card","1Add two =rolling=pierce= 3 cards","2Add one =rolling=stun= card","1Add one =rolling=disarm= and one =rolling=muddle= card","2Add one =+1=shield=1, Self card","1Ignore negative item effects and add one =+1= card"];
   }
   
   loadTinkererDeck() {
