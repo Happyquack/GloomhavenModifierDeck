e4e3bf09442c8af13aff901975b7ce7f5e16fafa
Jay Ozello <happyquackcrew@gmail.com>
2021-05-08 23:18:57 -0400

    Update page.js

--- scripts/page.js
+++ scripts/page.js
@@ -19,8 +19,8 @@ for (var i = 0; i < 18; i++) {
  if (num < 10) {num = "0" + num;}
  classIconImgs.push(document.createElement('img'));
  classIconImgs[i].src = ICONPATH + num + "icon.png";
- //classIconImgs[i].width = classIconWidths[i] + "";
- //classIconImgs[i].height = classIconHeights[i] + "";
+ classIconImgs[i].width = classIconWidths[i] + "";
+ classIconImgs[i].height = classIconHeights[i] + "";
 }
 
 var deck = new Deck();
