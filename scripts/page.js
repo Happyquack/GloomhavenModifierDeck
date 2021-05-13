e78b15b542cab87310cc7b393978a03a95a53b8d
Jay Ozello <happyquackcrew@gmail.com>
2021-05-13 10:50:04 -0400

    Update page.js

--- scripts/page.js
+++ scripts/page.js
@@ -53,7 +53,8 @@ var deckOfCards = deck.getPlayerDeck();
 
 function classSelectorClicked(event) {
  var image = event.target;
- var wrapper = document.getElementById(image.src.slice().split("/").pop());
+ var URL = document.getElementById(image.src.slice().split("/"));
+ var wrapper = URL[URL.length - 2];
  classIconImgs.forEach(el => {
   if (WHITELISTED_CLASSES.includes(el.id)) {
    el.style.borderColor = "black";
